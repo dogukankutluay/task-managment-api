c2f16b77f33fa458e596a094badf53e211daeb4b
Doğukan <dogu_kutluay_1999@hotmail.com>
2021-10-13 21:19:47 +0300

    some change

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const taskRepository = require("./app/repositories/taskRepository");
-const taskController = require("./app/controllers/taskController");
-const authController = require("./app/controllers/authController");
+const taskRepository = require('./app/repositories/taskRepository');
+const taskController = require('./app/controllers/taskController');
+const authController = require('./app/controllers/authController');
 
 const SERVER_PORT = 5000;
 const app = express();
@@ -24,8 +24,8 @@ taskController.mapRequestsToHandlers(app);
 // wrapped for testing
 if (!module.parent) {
   // staring the express server
-  app.listen(SERVER_PORT, function () {
-    console.log("Server is running at port :  ", SERVER_PORT);
+  app.listen(SERVER_PORT || 5000, function () {
+    console.log('Server is running at port :  ', SERVER_PORT);
   });
 }
 
